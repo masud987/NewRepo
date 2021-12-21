04749c1bd13ed184fa1304dfb3a42914e317649a
Use <Use@DESKTOP-IROU8IJ>
2021-12-21 21:32:03 +0600

    Add project files.

---
+++ ConsoleAppByZim/ConsoleAppByZim/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppByZim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 13;
+            int c = a + b;
+
+            Console.WriteLine(c);
+            Console.WriteLine(a);
+            Console.WriteLine("jim is here");
+            Console.ReadLine();
+        }
+    }
+}
